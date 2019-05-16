201bff7231e2fef7d26c26da22831fbdb8c9bb18
zglsjdflksdjfl <48215500+zglsjdflksdjfl@users.noreply.github.com>
2019-05-16 12:41:38 +0800

    自动化63-赵国梁-2160504081
    自动化63-赵国梁-2160504081

--- code.m
+++ code.m
@@ -8,6 +8,22 @@ test4=imread('test4.bmp');
 test5=imread('test5.png');
 test6=imread('test6.jpg');
 ########################
+S1=edge(test_im,'sobel');
+figure;
+imshow(S1);
+title('sobel算子');
+hold on
+P1=edge(test_im,'Prewitt');
+figure;
+imshow(P1);
+title('Prewitt算法');
+hold on
+C1=edge(test_im,'canny');
+figure;
+imshow(C1);
+title('canny算法');
+hold on
+######################
 function []=line_detect(test_im)
 
 #sobel算子
@@ -23,21 +39,6 @@ for k=1:length(lines)
     xy=[lines(k).point1;lines(k).point2];
     plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
 end
-
-#roberts算法
-P1=edge(test_im,'roberts');
-figure;
-imshow(P1);
-title('roberts算法');
-hold on
-[H,T,R]=hough(P1,'Theta',-90:0.5:89);
-P=houghpeaks(H,25);
-lines=houghlines(test_im,T,R,P);
-for k=1:length(lines)
-    xy=[lines(k).point1;lines(k).point2];
-    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-end
-
 #canny算法
 C1=edge(test_im,'canny');
 figure;
